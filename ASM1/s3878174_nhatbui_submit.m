f264fa816f74d0fdae037291bc8688f05cfa6380
nhat117 <s3878174@rmit.edu.vn>
2023-03-23 20:35:06 +0700

    * chore(s3878174_nhatbui.asv): delete file s3878174_nhatbui.asv
    * refactor(s3878174_nhatbui.m): update program title and quick start guide
    * refactor(s3878174_nhatbui.m): update instruction section with available unit conversion sections

--- ASM1/s3878174_nhatbui_submit.m
+++ ASM1/s3878174_nhatbui_submit.m
@@ -2,48 +2,51 @@
 % Student: Bui Minh Nhat-s3878174
 % This assignment submission is my own, original work.
 % Simple Unit Conversion Program
-%% Start here 
+%% Start
+% The program start at this section
+%Display program title and quick start guide
 clc, clearvars, close all
-disp('OENG1207 Digital Fundamentals - Individual Project Milstone') % Display program title
-disp('Welcome to Simple Unit Conversion Program')
-disp('Author: Bui Minh Nhat- s3878174')
-disp('------------------------------')
-disp('Quick start guide : ')
-disp('1.To use the program, select the desire section and press run section button in the toolbar of matlab')
-disp('2.To view the instruction, run the instruction section')
-disp('3.If you would like to quit the program, select the matlab Command Window and press Ctrl + C')
-disp('** The result are display with the accuracy of 2 decimal point')
-disp('------------------------------')
-disp('Please select the appropriate section to perform unit conversion: ')
+disp('OENG1207 Digital Fundamentals - Individual Project Milestone - Dr Alexandru C. Fechete');
+disp('Welcome to Simple Unit Conversion Program');
+disp('Author: Bui Minh Nhat - s3878174');
+disp('Welcome to Simple Unit Conversion Program');
+disp('------------------------------');
+disp('Quick start guide : ');
+disp('1.To use the program, select the desire section and press run section button in the toolbar of matlab');
+disp('2.To view the instruction, run the instruction section');
+disp('3.If you would like to quit the program, select the matlab Command Window and press Ctrl + C');
+disp('** The result are display with the accuracy of 2 decimal point');
+disp('------------------------------');
+disp('Please select and run the corresponding section to perform unit conversion: ');
 %% Instruction
 % The following section is to display the instruction for the user
 clc, clearvars, close all
 disp('Instruction Section');
-disp('1. To run the program, use the run section button in the toolbar of matlab')
-disp('2. Select the appropriate section to perform unit conversion')
-disp('3. If you would like to quit the program, select Command Window and press Ctrl + C')
-disp('** The result are display with the accuracy of 2 decimal point')
-disp('------------------------------')
-disp('The following are the available unit conversion sections:')
-disp('1.Temperature conversion')
-disp('To convert temperature from F-> C run the F-> C section')
-disp('To convert temperature from C-> F run the C-> section')
-disp('------------------------------')
-disp('2.Length and distance conversion')
-disp('To convert length from cm-> inch run the cm-> inch section')
-disp('To convert length from inch-> cm run the inch-> cm section')
-disp('To convert length from m-> ft run the m-> ft section')
-disp('To convert length from ft-> m run the ft-> m section')
-disp('To convert length from km-> miles run the km-> miles section')
-disp('To convert length from miles-> km run the miles-> km section')
-disp('------------------------------')
-disp('3.Mass conversion')
-disp('To convert mass from grams-> ounces run the grams-> ounces section')
-disp('To convert mass from ounces-> grams run the ounces-> grams section')
-disp('To convert mass from kg-> pounds run the kg-> pounds section')
-disp('To convert mass from pounds-> kg run the pounds-> kg section')
-disp('To convert mass from ton(met)-> ton(imp) run the ton(met)-> ton(imp) section')
-disp('To convert mass from ton(imp)-> ton(met) run the ton(imp)-> ton(met) section')
+disp('1. To run the program, use the run section button in the toolbar of matlab');
+disp('2. Select the appropriate section to perform unit conversion');
+disp('3. If you would like to quit the program, select Command Window and press Ctrl + C');
+disp('** The result are display with the accuracy of 2 decimal point');
+disp('------------------------------');
+disp('The following are the available unit conversion sections:');
+disp('1.Temperature conversion');
+disp('To convert temperature from F-> C run the F-> C section');
+disp('To convert temperature from C-> F run the C-> F section');
+disp('------------------------------');
+disp('2.Length and distance conversion');
+disp('To convert length from cm-> inch run the cm-> inch section');
+disp('To convert length from inch-> cm run the inch-> cm section');
+disp('To convert length from m-> ft run the m-> ft section');
+disp('To convert length from ft-> m run the ft-> m section');
+disp('To convert length from km-> miles run the km-> miles section');
+disp('To convert length from miles-> km run the miles-> km section');
+disp('------------------------------');
+disp('3.Mass conversion');
+disp('To convert mass from grams-> ounces run the grams-> ounces section');
+disp('To convert mass from ounces-> grams run the ounces-> grams section');
+disp('To convert mass from kg-> pounds run the kg-> pounds section');
+disp('To convert mass from pounds-> kg run the pounds-> kg section');
+disp('To convert mass from ton(met)-> ton(imp) run the ton(met)-> ton(imp) section');
+disp('To convert mass from ton(imp)-> ton(met) run the ton(imp)-> ton(met) section');
 
 %% Temperature conversion
 %% F->C
