033f722b4d945d52670c9ca2fd2c6cb5b924b0d3
nhat117 <s3878174@rmit.edu.vn>
2023-03-30 23:55:22 +0700

    * refactor(lab2.asv): change pixel range for red color * refactor(lab2.asv): change pixel range for yellow color * refactor(lab2.asv): change pixel range for blue color * refactor(lab2.asv): change pixel range for green color * refactor(lab2.asv): change pixel range for white color * refactor(lab2.asv): change pixel range for black color * refactor(lab2.asv): change pixel range for cyan color * refactor(lab2.asv): change pixel range for magenta color * refactor(lab2.asv): change pixel range for orange color * feat(lab2.asv): add input for image path and convert input color to lowercase
    * refactor(lab2.asv): improve error handling for invalid color input
    * feat(lab2.asv): add functionality to change specified color to black in the image
    
    * feat(lab2.m): add support for changing color to black based on user input
    * refactor(lab2.m): change variable name from input_image to color_change
    * refactor(lab2.m): convert user input color to lowercase
    * refactor(lab2.m): change mask variable names to lowercase
    * refactor(lab2.m): change mask variable names to match color names
    * refactor(lab2.m): add error handling for invalid color input
    * refactor(lab2.m): add status variable to handle error status
    * refactor(lab2.m): change nested for loop to handle mask variable

--- Group Lab/Lab2/lab2.m
+++ Group Lab/Lab2/lab2.m
@@ -1,65 +1,69 @@
-%% Write a program in matlab to change the color of the image
 clc, clearvars, close all
 % Read the image
 display('Welcome to color changing program')
-color_change = input('Please enter a color to change it to black :','s')
+% input_image = ('Please enter image path: ', 's')
+color_change = lower(input('Please enter a color to change it to black :', 's')); % Get input color and convert it to lower case
+% rgb = imread(input_image)
 rgb = imread("coloredChips.png");
-figure(1)
-subplot(1,2,1)
-imshow(rgb)
+status = 0; %Normal status
+
 % Check row and cols size
-row_size = size(rgb,1);
-col_size = size(rgb,2);
-% dest = [0,0,0]
-% Define pixel range for red blue green magenta cyan, white
-red = [rgb(:,:,1) >= 200 & rgb(:,:,2) >= 5 & rgb(:,:,2) <= 50 & rgb(:,:,3) <= 100]; 
-yellow = [rgb(:,:,1) >= 200 & rgb(:,:,2) >= 100 & rgb(:,:,3) <= 50];
-blue  = [rgb(:,:,1) <= 25 & rgb(:,:,2) > 40 & rgb(:,:,2) <= 120 & rgb(:,:,3) > 130];
-green  = [rgb(:,:,1) < 80 & rgb(:,:,2) <= 200 & rgb(:,:,2) > 120 & rgb(:,:,3) <= 140 & rgb(:,:,3) > 55];
-white = [rgb(:,:,1) >= 57 & rgb(:,:,2) >= 55 & rgb(:,:,3) >= 55];
-black = [rgb(:,:,1) >= 5 & rgb(:,:,1) <= 80 & rgb(:,:,2) >= 5 & rgb(:,:,2) <= 80  & rgb(:,:,3) >= 5 & rgb(:,:,3) <= 80];
-cyan = [rgb(:,:,1) >= 20 & rgb(:,:,1) < 80  & rgb(:,:,2) >= 100 & rgb(:,:,2) <= 245 & rgb(:,:,3) >= 100 & rgb(:,:,3) <= 245];
-magenta = [rgb(:,:,1) >= 100 & rgb(:,:,1) <= 245 & rgb(:,:,2) >= 20 & rgb(:,:,2) < 110 & rgb(:,:,3) >= 100 & rgb(:,:,3) <= 245];
+row_size = size(rgb, 1);
+col_size = size(rgb, 2);
+
+% Define pixel range for red blue green magenta cyan, white, orange, yellow, and black
+red = rgb(:, :, 1) >= 200 & rgb(:, :, 2) >= 5 & rgb(:, :, 2) <= 50 & rgb(:, :, 3) <= 100;
+yellow = rgb(:, :, 1) >= 200 & rgb(:, :, 2) >= 100 & rgb(:, :, 3) <= 50;
+blue = rgb(:, :, 1) <= 25 & rgb(:, :, 2) > 40 & rgb(:, :, 2) <= 120 & rgb(:, :, 3) > 130;
+green = rgb(:, :, 1) < 80 & rgb(:, :, 2) <= 200 & rgb(:, :, 2) > 120 & rgb(:, :, 3) <= 140 & rgb(:, :, 3) > 55;
+white = rgb(:, :, 1) >= 57 & rgb(:, :, 2) >= 55 & rgb(:, :, 3) >= 55;
+black = rgb(:, :, 1) >= 5 & rgb(:, :, 1) <= 80 & rgb(:, :, 2) >= 5 & rgb(:, :, 2) <= 80 & rgb(:, :, 3) >= 5 & rgb(:, :, 3) <= 80;
+cyan = rgb(:, :, 1) >= 20 & rgb(:, :, 1) < 80 & rgb(:, :, 2) >= 100 & rgb(:, :, 2) <= 245 & rgb(:, :, 3) >= 100 & rgb(:, :, 3) <= 245;
+magenta = rgb(:, :, 1) >= 100 & rgb(:, :, 1) <= 245 & rgb(:, :, 2) >= 20 & rgb(:, :, 2) < 110 & rgb(:, :, 3) >= 100 & rgb(:, :, 3) <= 245;
+orange = rgb(:, :, 1) >= 200 & rgb(:, :, 2) >= 80 & rgb(:, :, 2) <= 200 & rgb(:, :, 3) <= 50;
 
 switch color_change
     case 'red'
         dest = red;
-        rgb(repmat(dest,[1,1,3]))=0; % Replace the corresponding mask to 0
     case 'blue'
         dest = blue;
-        rgb(repmat(dest,[1,1,3]))=0; % Replace the corresponding mask to 0
     case 'green'
         dest = green;
-        rgb(repmat(dest,[1,1,3]))=0; % Replace the corresponding mask to 0
     case 'magenta'
         dest = magenta;
-        rgb(repmat(dest,[1,1,3]))=0; % Replace the corresponding mask to 0
     case 'cyan'
         dest = cyan;
-        rgb(repmat(dest,[1,1,3]))=0; % Replace the corresponding mask to 0
     case 'white'
         dest = white;
-        rgb(repmat(dest,[1,1,3]))=0; % Replace the corresponding mask to 0
     case 'orange'
         dest = orange;
-        rgb(repmat(dest,[1,1,3]))=0; % Replace the corresponding mask to 0
     case 'yellow'
         dest = yellow;
-        rgb(repmat(dest,[1,1,3]))=0; % Replace the corresponding mask to 0
     case 'black'
         dest = black;
-        rgb(repmat(dest,[1,1,3]))=0; % Replace the corresponding mask to 0
-    otherwise % invalid resposnse
-        disp('Invalid response, the program will exit now')
+    otherwise % invalid respose handling
+        disp('Invalid color, the program will exit now')
+        status = -1; % The program encounter error
+        return
 end
 
-% Change the color to black
-disp('Changing the color to black')
-disp('Please wait...')  
-display(dest)
-rgb(repmat(dest,[1,1,3]))=0;
-
-% Display the blue
-figure(1)
-subplot(1,2,2)
-imshow(rgb)
+if status == 0 % Check if the program encounter any error
+    % Display the original image
+    figure(1)
+    subplot(1, 2, 1)
+    imshow(rgb)
+    % Change the color to black
+    fprintf('Changing the %s color in the picture to black \n', color_change)
+    disp('Please wait...')
+    for i = 1:row_size
+        for j = 1:col_size
+            if dest(i, j)
+                rgb(i, j, :) = [0 0 0];
+            end
+        end
+    end
+    % Display the transform image
+    figure(1)
+    subplot(1, 2, 2)
+    imshow(rgb)
+end
