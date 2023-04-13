a0c04d90e40d59e72ad877e20cdf61c4e780b460
nhat117 <s3878174@rmit.edu.vn>
2023-04-13 10:31:27 +0700

    Refractor elee

--- Group Lab/Lab1/lab1.m
+++ Group Lab/Lab1/lab1.m
@@ -1,20 +1,30 @@
-%% Week2 Ex1 Task
-clc, clearvars, close all, format long
+% OENG1207 Digital Fundamenta
+% Author : Nhat Bui(s3878174), Binh Nguyen(s3979462), Thao Trinh (s3979297)
+% This assignment is our team own original work
+%% Lab 1 Part 1
+clc, clearvars, close all, format long % Clear command windows, variable and close all matlab windows
+% Display program information
+display('OENG1207 Digital Fundamental')
+display('Author : Nhat Bui(s3878174), Binh Nguyen(s3979462), Thao Trinh (s3979297)')
+display('Lab 1 Part 1')
 d = 183; %metters
 theta_deg = 45;
 theta_rad = deg2rad(theta_deg); %rad;
 g = 9.8; %m/s^2;
 v0_ms = sqrt((d * g) / sin(2 * theta_rad)); % the unit is in m/s;
 v0_kmh = convvel(v0_ms, 'm/s', 'km/h');
-fprintf('The initial velocity is %.3f km/h', v0_kmh);
+fprintf('The initial velocity is %.3f km/h \n', v0_kmh);
 
-%% Week3
-clc, clearvars, close all
-
+%% Lab 1 Part 2
+clc, clearvars, close all  % Clear command windows, variable and close all matlab windows
+% Display program information
+display('OENG1207 Digital Fundamental')
+display('Lab 1 Part 2')
+display('Author : Nhat Bui(s3878174), Binh Nguyen(s3979462), Thao Trinh (s3979297)')
 g = 9.8; % Gravitational velocity in m/s^2
 theta_deg = [35, 45, 55]; % Degrees, define a vector of different angles
-% % at theta = 45 maximum velocity equal 125km/h2
-% Theta 45 is second element in the theta_rad vector  ';a,v'
+%  at theta = 45 maximum velocity equal 125km/h2
+% Theta 45 is second element in the theta_rad vector
 x_min = 183; % Narrowest disstance at the canyon
 v0_max = 125; % Maximum velocity in km/h
 
@@ -31,26 +41,26 @@ fprintf('The initial height is %.3f m \n', h_initial)
 step = 0.01; % Time step (explain more) because 2 decimal digit 
 t_end = 8.42 + 0.1; % t_max to reach the land from 3 launching angle
 t = 0:step:t_end; % Time vector 
-x = v0_max * cos(theta_rad)' * t;
-y = h_initial + v0_max .* t .* sin(theta_rad)' - 0.5 * g * (t .^ 2);
+x = v0_max * cos(theta_rad)' * t; % Formular to calculate x
+y = h_initial + v0_max .* t .* sin(theta_rad)' - 0.5 * g * (t .^ 2); % Formular to calculate the y value base on h_inital , vo_max
 
 % Plot the trajectory
 figure(1)
-
-%Don
+% Plot all of the three line
  i =1;
+ plot(x(i, :), y(i, :)) % Access the first angle
+ hold on %Keep the line on the same plot
+ i = 2; % Plot the second angle
  plot(x(i, :), y(i, :))
- hold on
- i = 2;
+ hold on % Keep the line on the same plot
+ i = 3; % Plot the third angle
  plot(x(i, :), y(i, :))
- hold on
- i = 3;
- plot(x(i, :), y(i, :))
- hold on
+ hold on % Keep the line on the same plot
+ 
 
 % Plot configuration
 legend('35 degree', '45 degree', '55 degree');
 axis([0 210 0 140]); % define the range for the axis, use positive range only
 title('Height vs Displacement respective to time for different launching angle'); % title name for the graph
-xlabel('x-displacement in meters');
-ylabel('y-height in meters'); % label name for x, y axis
+xlabel('x-displacement in meters'); % label name for x axis
+ylabel('y-height in meters'); % label name for y axis
